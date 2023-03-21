7555d4e0efc69d32cfaf12a9b83d912e1d251fa7
phdfreitas <pedro.hnd.freitas@gmail.com>
2023-03-21 08:59:22 -0300

    Adicionando estilos e evitando warning no php

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio Kidopi</title>
 
+    <link rel="stylesheet" href="css/style.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <div class="row">
+        <div class="row" id="formulario">
             <p>Selecione o país que você quer verificar os dados.</p>
             <form>
                 <label for="pais">
-                    <input type="radio" name="pais" class="pais-info1" value="Brazil">
+                    <input type="radio" name="pais" id="brasil" class="pais-info1" value="Brazil">
                     Brasil
                 </label>
 
@@ -35,10 +37,10 @@
         </div>
         
         <div class="row">
-            <h1 class="titulo">
-                Informações sobre <span>País</span>
+            <h1 class="titulo" id="informacao">
+                Informações sobre o <span id="paisInformacao">País</span>
             </h1>
-            <table class="table">
+            <table class="table" id="dadosCovid">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
@@ -80,8 +82,12 @@
                         endif;
                     ?>
                     <p>
-                        Total de confirmados: <?= htmlspecialchars($totalConfirmados); ?>
-                        Total de mortes <?= htmlspecialchars($totalMortos); ?>
+                        Total de confirmados: 
+                        <?= 
+                            isset($_GET['pais']) ? htmlspecialchars($totalConfirmados) : 0; ?>
+                        
+                        Total de mortes <?= 
+                            isset($_GET['pais']) ? htmlspecialchars($totalMortos) : 0; ?>
                     </p>
                 </tbody>
             </table>
@@ -97,6 +103,6 @@
             </div>
         </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
+    <script src="js/script.js"></script>
 </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,23 @@
+let url = window.location.href.split('?');
+
+let dadosCovid = document.getElementById('dadosCovid');
+let informacao = document.getElementById('informacao');
+let paisInformacao = document.getElementById('paisInformacao');
+
+const alteraNomePais = (nomePais) => {
+    if(nomePais === "Brazil"){
+        return "Brasil";
+    }
+    else if(nomePais === "Canada"){
+        return "Canadá";
+    }
+}
+
+if (url.length === 1) {
+    dadosCovid.hidden = true;
+    informacao.innerHTML = 'Nenhum país foi selecionado';
+}
+else{
+    paisInformacao.innerHTML = alteraNomePais(url[1].substring(5));
+}
+
